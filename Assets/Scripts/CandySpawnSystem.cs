0e6b2d5ed62f67a1ec9ed44b0ecc8275fa457082
dicenull <dicenull.dev@gmail.com>
2023-12-06 23:50:08 +0900

    ✨キャンディーをたくさん生成した

--- Assets/Scripts/CandySpawnSystem.cs
+++ Assets/Scripts/CandySpawnSystem.cs
@@ -2,30 +2,33 @@ using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
 using Unity.Mathematics;
+using Unity.Rendering;
 using Unity.Transforms;
 
 [UpdateInGroup(typeof(InitializationSystemGroup))]
 public partial struct CandySpawnSystem : ISystem
 {
-    public void OnCreate(ref SystemState state)
-      => state.RequireForUpdate<Config>();
+  public void OnCreate(ref SystemState state)
+    => state.RequireForUpdate<Config>();
 
-    public void OnUpdate(ref SystemState state)
+  [BurstCompile]
+  public void OnUpdate(ref SystemState state)
+  {
+    var config = SystemAPI.GetSingleton<Config>();
+
+    var instances = state.EntityManager.Instantiate
+      (config.Prefab, 1, Allocator.Temp);
+    var rand = new Random((uint)math.floor(SystemAPI.Time.ElapsedTime * 1000) + 100);
+    foreach (var entity in instances)
     {
-        var config = SystemAPI.GetSingleton<Config>();
+      var xform = SystemAPI.GetComponentRW<LocalTransform>(entity);
 
-        var instances = state.EntityManager.Instantiate
-          (config.Prefab, config.SpawnCount, Allocator.Temp);
-
-        var rand = new Random(1000);
-        foreach (var entity in instances)
-        {
-            var xform = SystemAPI.GetComponentRW<LocalTransform>(entity);
-
-            var v = rand.NextFloat2(-1, 1);
-            xform.ValueRW = LocalTransform.FromPosition(math.float3(v.x, 0, v.y) * config.SpawnRadius);
-        }
-
-        state.Enabled = false;
+      var v = rand.NextFloat2(-1, 1);
+      var h = rand.NextFloat(.5f, 1);
+      var size = rand.NextFloat(.1f, .5f);
+      xform.ValueRW = LocalTransform
+        .FromPosition(math.float3(v.x, h, v.y) * config.SpawnRadius)
+        .WithScale(size);
     }
+  }
 }
